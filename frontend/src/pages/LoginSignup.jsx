a9f9122ba7df14c6e81bfd2012f446d7375a3c27
Sidrahhh <157208115+Sidrahhh@users.noreply.github.com>
2024-11-17 10:07:36 +0530

    Update LoginSignup.jsx

--- frontend/src/pages/LoginSignup.jsx
+++ frontend/src/pages/LoginSignup.jsx
@@ -26,7 +26,7 @@ const LoginSignup = () => {
     const url = isLogin ? '/login' : '/signup';
 
     try {
-      const response = await fetch(`https://quad-core-web-dev-f49v-new.vercel.app/${url}`, {
+      const response = await fetch(`https://quad-core-web-dev-f49v-new.vercel.app${url}`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
